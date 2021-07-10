8b9a88ba4c342a0aa0e7b19b9425ade66c49066a
Katie <katherinelane11@gmail.com>
2021-07-10 16:41:02 -0400

    Starts up basic react app

--- src/index.js
+++ src/index.js
@@ -1,103 +1,17 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
-const { makeExecutableSchema } = require('graphql-tools');
-const cors = require('cors');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-// initialize people datastore
-let people = [];
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-// load seed people
-const https = require('https')
-const options = {
-    hostname: 'randomuser.me',
-    path: '/api/?results=100&inc=name,email,picture',
-    method: 'GET'
-}
-
-const req = https.request(options, res => {
-    console.log('Seeding data...');
-    let data = '';
-    res.on('data', chunk => {
-        data += chunk;
-    });
-    res.on('end', () => {
-        people = JSON.parse(data).results;
-        people.forEach((p,idx) => p.id = idx);
-        console.log('Data Seeded!');
-    })
-
-});
-req.end();
-
-// The GraphQL schema in string form
-const typeDefs = `
-  type Query { 
-      people: [Person] 
-      person(id: ID!): Person
-    }
-  type Mutation {
-      editPerson(id: ID!, payload:EditPerson): Person!
-  }
-  input EditPerson { title: String, first: String last: String, email: String}
-  type Name { title: String, first: String, last: String}
-  type Picture { large: String, medium: String, thumbnail: String}
-  type Person { id: ID!, name: Name, email: String, picture: Picture }
-`;
-
-// The resolvers
-const resolvers = {
-    Query: {
-        people: () => people,
-        person: (_, args) => {
-            const idx = people.findIndex(p => p.id === parseInt(args.id));
-            if (idx < 0) {
-                throw error('Person not found');
-            }
-            return people[idx];
-        }
-    },
-    Mutation: {
-        editPerson: (_, args) => {
-            const idx = people.findIndex(p => p.id == parseInt(args.id));
-            if (idx < 0) {
-                throw error('Person not found');
-            }
-            if(args.payload.title) {
-                people[idx].name.title = args.payload.title;
-            }
-            if(args.payload.first) {
-                people[idx].name.first = args.payload.first;
-            }
-            if(args.payload.last) {
-                people[idx].name.last = args.payload.last;
-            }
-            if(args.payload.email) {
-                people[idx].email = args.payload.email;
-            }
-            return people[idx];
-        }
-    }
-};
-
-// Put together a schema
-const schema = makeExecutableSchema({
-    typeDefs,
-    resolvers,
-});
-
-// Initialize the app
-const app = express();
-
-app.use(cors());
-
-// The GraphQL endpoint
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-
-// GraphiQL, a visual editor for queries
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
-
-// Start the server
-app.listen(8080, () => {
-    console.log(`Welcome to the Firstbase Frontend Coding Challenge API\n GraphiQL: http://localhost:8080/graphiql\n GOOD LUCK!`);
-});
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
